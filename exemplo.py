dcf17485f498ba0b537058a47a11958fc6e71f3f
Paulo-py <101109486+Paulo-py@users.noreply.github.com>
2025-05-09 22:25:38 -0300

    atividade de funções

---
+++ exemplo.py
@@ -0,0 +1,9 @@
+# Este é um exemplo simples de programa Python
+print("Olá, Mundo!")
+
+# Função básica
+def saudacao(nome):
+    return f"Olá, {nome}! Bem-vindo ao Python!"
+
+# Exemplo de uso da função
+print(saudacao("Aluno"))
